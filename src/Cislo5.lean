c80b22184629aa47759f5169ee6188b3aeddf800
madvorak <dvorakmartinbridge@seznam.cz>
2023-02-27 22:06:42 +0100

    soucet lichych cisel vzestupne

--- src/Cislo5.lean
+++ src/Cislo5.lean
@@ -5,11 +5,29 @@ import Mathlib.Tactic.LibrarySearch
 import Mathlib.Tactic.Ring
 
 
-theorem soucet_prvnich_N_lichych (N : Nat) : soucet (prvnich_N_lichych N) = N * N := by
+lemma soucet_prvnich_N_lichych_sestupne (N : Nat) : soucet (prvnich_N_lichych_sestupne N) = N * N := by
   induction' N with n ih
   · rfl
   rw [Nat.succ_mul, Nat.mul_succ]
-  unfold prvnich_N_lichych
+  unfold prvnich_N_lichych_sestupne
   unfold soucet
   rw [ih, dva_krat]
   rw [add_comm, add_assoc, add_assoc]
+
+lemma soucet_dvou_seznamu (x y : List Nat) : soucet (x ++ y) = soucet x + soucet y := by
+  induction' x with x₀ xs ih
+  · simp
+  convert_to x₀ + soucet (xs ++ y) = x₀ + soucet xs + soucet y
+  rw [ih, add_assoc]
+
+lemma obrat_zachovava_soucet (l : List Nat) : soucet (obrat l) = soucet l := by
+  induction' l with hlava zbytek indukcni
+  · rfl
+  unfold obrat
+  rw [soucet_dvou_seznamu, indukcni, add_comm]
+  rfl
+
+theorem soucet_prvnich_N_lichych (N : Nat) : soucet (prvnich_N_lichych N) = N * N := by
+  simp [prvnich_N_lichych]
+  rw [obrat_zachovava_soucet]
+  apply soucet_prvnich_N_lichych_sestupne
