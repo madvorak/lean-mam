50a9a9207af9d0df21f9729d722237ffc82e97a6
madvorak <dvorakmartinbridge@seznam.cz>
2023-03-02 15:43:10 +0100

    veci pro treti cislo

--- mam/Cislo3.lean
+++ mam/Cislo3.lean
@@ -3,29 +3,46 @@ import Mathlib.Tactic.FieldSimp
 import Mathlib.Tactic.LibrarySearch
 
 
-theorem dva_krat (n : Nat) : 2 * n = n + n := by ring
+theorem dva_krat (n : Nat) : 2 * n = n + n := two_mul n
 
-theorem soucet_na_druhou (x y : ℚ) : (x + y) ^ 2 = x ^ 2 + 2 * x * y + y ^ 2 := by ring
+theorem tri_krat (n : Nat) : 3 * n = n + n + n := by
+  convert_to 3 * n = 2 * n + n
+  exact symm (dva_krat n)
+  exact Nat.succ_mul 2 n
 
-theorem soucet_na_treti (x y : ℝ) : (x + y) ^ 3 = x^3 + 3 * x^2 * y + 3 * x * y^2 + y^3 := by ring
+theorem tri_krat' (n : Nat) : 3 * n = n + n + n := by ring
 
-theorem rozdil_na_treti (x y : ℝ) : (x - y) ^ 3 = x^3 - 3 * x^2 * y + 3 * x * y^2 - y^3 := by ring
+theorem soucet_na_druhou (x y : ℤ) : (x + y) ^ 2 = x^2 + 2*x*y + y^2 := by ring
+
+theorem rozdil_na_treti (x y : ℚ) : (x - y) ^ 3 = x^3 - 3*x^2*y + 3*x*y^2 - y^3 := by ring
 
 theorem rozdil_patych_mocnin (x y : ℝ) : x^5 - y^5 = (x - y) * (x^4 + x^3*y + x^2*y^2 + x*y^3 + y^4) := by ring
 
-theorem na_druhou_deleno (x : ℝ) (xnz : x ≠ 0) : x^2 / x = x := by
+example (x y : ℝ) : (x + y) ^ 2 - (x - y) ^ 2 = 4 * x * y := by ring
+
+example (n : Nat) : 2 ^ (n+3) = 8 * 2^n := by ring
+
+example (n : Nat) (n_je_pet : n = 5) : n - 1 = 4 := by
+  rw [n_je_pet]
+
+example (n : Nat) (n_je_pet : 5 = n) : n - 1 = 4 := by
+  rw [symm n_je_pet]
+
+example (n : Nat) (n_je_pet : 5 = n) : n - 1 = 4 := by
+  rw [←n_je_pet]
+
+example (a b c : ℝ) (a_je_dva : a = 2) (b_je_tri : b = 3) (c_je_pet : c = 5) : a + b = c := by
+  rw [a_je_dva, b_je_tri, c_je_pet]
+  ring
+
+example (x : ℝ) (xnz : x ≠ 0) : x^2 / x = x := by
   field_simp
   ring
 
-theorem plus_prevracena (x : ℝ) : x + 1/x = (x^2 + 1) / x := by
-  ring_nf
-  congr
-  convert_to x = x * x * x⁻¹
+theorem plus_prevracena (x : ℝ) (xnz : x ≠ 0) : x + 1/x = (x^2 + 1) / x := by
+  field_simp
   ring
-  simp
 
-
-example (x y : ℝ) : (x + y) ^ 2 - (x - y) ^ 2 = 4 * x * y := by ring
 
 example (x : ℝ) (xpos : x > 0) : x + 1/x ≥ 2 := by
   have : (x - 1) ^ 2 ≥ 0
---
+++ mam/Zadani3.lean
@@ -0,0 +1,3 @@
+import mam.Cislo3
+
+theorem na_druhou (n : Nat) : n ^ 2 = n * n := sorry
