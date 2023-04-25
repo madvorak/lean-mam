45d7220816557ee98786f4580afeb18dbe81df49
madvorak <dvorakmartinbridge@seznam.cz>
2023-04-25 20:51:14 +0200

    pridana Uloha 9

--- mam/Zadani1.lean
+++ mam/Zadani1.lean
@@ -132,3 +132,9 @@ vypis_splnujici_do je_dokonale_cislo n
 
 #eval seznam_dokonalych_cisel_do 500
 #eval seznam_dokonalych_cisel_do 10000
+
+
+def maximum_z_krychle (g : Nat → Nat → Nat → Nat) (a : Nat) : Nat := 0 -- TODO
+
+#eval maximum_z_krychle (fun x y z => x + y - z) 10    /- `18` -/
+#eval maximum_z_krychle (fun x y z => x * (6-x) * y * (4-y) * z * (10-z)) 7    /- `900` -/
