b2d80961eb2706cc937c6a150d93b2935a37937e
madvorak <dvorakmartinbridge@seznam.cz>
2023-05-04 17:16:44 +0200

    redundant lean_lib removed

--- lakefile.lean
+++ lakefile.lean
@@ -6,12 +6,6 @@ package «lean-mam» where
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
 
-lean_lib «mam/Cislo1»
-lean_lib «mam/Cislo2»
-lean_lib «mam/Cislo3»
-lean_lib «mam/Cislo4»
-lean_lib «mam/Cislo5»
-
 @[default_target]
 lean_lib all where
   moreLeanArgs := #["-DautoImplicit=false"]
