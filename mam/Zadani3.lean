5c5dd4b614304971abb78d9cd5b235789bca070b
madvorak <dvorakmartinbridge@seznam.cz>
2023-09-02 17:13:36 +0200

    ulohy do tretiho dilu

--- mam/Zadani3.lean
+++ mam/Zadani3.lean
@@ -1,3 +1,10 @@
 import mam.Cislo3
 
 theorem na_druhou (n : Nat) : n ^ 2 = n * n := sorry
+
+example (a b c d : ℕ) (a_je : a = b + d) (b_je : b = a * a) (c_je : c = b + d) (d_je : d = c * c) :
+  b ^ d = d ^ b := sorry
+
+example {x : ℚ} : x^2 - 6*x + 10 ≥ 0 := sorry
+
+example {a b : ℝ} (ha : 0 < a) (hb : 0 < b) : a / b^2 + b / a^2 ≥ 1 / a + 1 / b := sorry
